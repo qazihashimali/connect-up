c4d778643de6978eb0302e2eb154684a118f3a55
QAXI-HASHIM-ALI <qaxihashimali@gmail.com>
2026-01-07 15:23:22 +0500

    Update the PORT issue

--- server/server.js
+++ server/server.js
@@ -92,9 +92,6 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-// Start server
-// server.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
-export default server;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
